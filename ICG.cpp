49f8df3a707a9eab67ce6b75cc9d8bb5ed131e9d
Ekansh Vishnoi <ekanshvishnoi6@gmail.com>
2025-08-15 19:57:28 +0530

    create TAC and get it optimised

--- ICG.cpp
+++ ICG.cpp
@@ -60,3 +60,68 @@ vector<string> convertToPostfix(const string& expr) {
 
     return result;
 }
+
+
+vector<string> createTAC(const vector<string>& postfix, map<string, int>& table) {
+    stack<string> stk;
+    vector<string> tac;
+
+    for (const auto& token : postfix) {
+        if (isalnum(token[0]) && token.length() == 1) {
+            stk.push(token);
+        } else {
+            string right = stk.top(); stk.pop();
+            string left = stk.top(); stk.pop();
+            string temp = generateTempVar(table);
+            tac.push_back(temp + " = " + left + " " + token + " " + right);
+            stk.push(temp);
+        }
+    }
+    return tac;
+}
+
+void applyConstantFolding(vector<string>& tacList) {
+    for (auto& stmt : tacList) {
+        stringstream ss(stmt);
+        string lhs, eq, left, op, right;
+        ss >> lhs >> eq >> left >> op >> right;
+
+        if (isdigit(left[0]) && isdigit(right[0])) {
+            int a = stoi(left), b = stoi(right), res = 0;
+            if (op == "+") res = a + b;
+            else if (op == "-") res = a - b;
+            else if (op == "*") res = a * b;
+            else if (op == "/") res = b != 0 ? a / b : 0;
+            stmt = lhs + " = " + to_string(res);
+        } else if ((left == "0" && op == "*") || (right == "0" && op == "*")) {
+            stmt = lhs + " = 0";
+        }
+    }
+}
+
+void eliminateDeadCode(vector<string>& tacList) {
+    set<string> used;
+    for (const auto& stmt : tacList) {
+        stringstream ss(stmt);
+        string lhs, eq, left, op, right;
+        ss >> lhs >> eq >> left;
+        if (ss >> op >> right) {
+            if (!isdigit(left[0])) used.insert(left);
+            if (!isdigit(right[0])) used.insert(right);
+        } else {
+            if (!isdigit(left[0])) used.insert(left);
+        }
+    }
+
+    vector<string> optimized;
+    for (const auto& stmt : tacList) {
+        string lhs = stmt.substr(0, stmt.find('=') - 1);
+        if (used.count(lhs) || lhs == "result") {
+            optimized.push_back(stmt);
+        } else {
+            cout << "Eliminated Dead Code: " << stmt << endl;
+        }
+    }
+
+    tacList = optimized;
+}
